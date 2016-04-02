f7f8b52f00687bb4dafac35ac6a38089e9ceff20
Ben Yan <yanwumo@outlook.com>
2016-04-02 15:40:14 +0800

    update

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php require_once("header.php"); ?>
+<?php
+$page = isset($_GET["page"]) && $_GET["page"] >= 1 ? $_GET["page"] : 1;
+
+$statement = $dbh->prepare("SELECT COUNT(*) FROM goods");
+$statement->execute();
+$row = $statement->fetch();
+$goods_per_page = 12;
+$total_goods = $row["COUNT(*)"];
+$total_page = ceil($total_goods / $goods_per_page);
+if ($total_page == 0) $total_page = 1;
+$lower_bound = ($page - 1) * $goods_per_page;
+if ($total_page > 9) {
+    $need = true;
+    $need_left = false;
+    $need_right = false;
+    if ($page >= 5) {
+        $need_left = true;
+        $left_bound = $total_page - 5;
+    }
+    if ($page <= $total_page - 4) {
+        $need_right = true;
+        $right_bound = 6;
+    }
+} else {
+    $need = false;
+}
+?>
 
 <div class="col-md-2">
     <div class="list-group">
@@ -201,27 +228,107 @@
     <div class="row">
         <div class="col-md-12">
             <ul class="pagination">
-                <li>
-                    <a href="#">Prev</a>
-                </li>
-                <li>
-                    <a href="#">1</a>
-                </li>
-                <li>
-                    <a href="#">2</a>
-                </li>
-                <li>
-                    <a href="#">3</a>
-                </li>
-                <li>
-                    <a href="#">4</a>
-                </li>
-                <li>
-                    <a href="#">5</a>
-                </li>
-                <li>
-                    <a href="#">Next</a>
-                </li>
+                <!-- Prev Page -->
+                <?php if ($page == 1) { ?>
+                    <li class="disabled">
+                        &laquo;
+                    </li>
+                <?php } else { ?>
+                    <li>
+                        <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=<?php echo $page - 1; ?>">&laquo;</a>
+                    </li>
+                <?php } ?>
+
+                <!-- Pages -->
+                <?php
+                if (!$need) {
+                    for ($i = 1; $i <= $total_page; $i++) {
+                        if ($i != $page) {
+                            ?>
+                            <li>
+                                <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                            </li>
+                            <?php
+                        } else {
+                            ?>
+                            <li class="active">
+                                <a><?php echo "$i"; ?></a>
+                            </li>
+                            <?php
+                        }
+                    }
+                } else {
+                    if ($need_left && !$need_right) {
+                        ?>
+                        <li>
+                            <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=1&category=<?php echo $category; ?>"><?php echo "1..."; ?></a>
+                        </li>
+                        <?php
+                        for ($i = $left_bound; $i <= $total_page; $i++) {
+                            if ($i != $page) {
+                                ?>
+                                <li>
+                                    <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=<?php echo $i; ?>&category=<?php echo $category; ?>"><?php echo $i; ?></a>
+                                </li>
+                                <?php
+                            } else {
+                                ?>
+                                <li class="active">
+                                    <a><?php echo "$i"; ?></a>
+                                </li>
+                                <?php
+                            }
+                        }
+                    } else if (!$need_left && $need_right) {
+                        for ($i = 1; $i <= $right_bound; $i++) {
+                            if ($i != $page) {
+                                ?>
+                                <li>
+                                    <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=<?php echo $i; ?>&category=<?php echo $category; ?>"><?php echo $i; ?></a>
+                                </li>
+                                <?php
+                            } else {
+                                ?>
+                                <li class="active">
+                                    <a><?php echo "$i"; ?></a>
+                                </li>
+                                <?php
+                            }
+                        }
+                        ?>
+                        <li>
+                            <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=<?php echo $total_page; ?>&category=<?php echo $category; ?>"><?php echo "...$totalPage"; ?></a>
+                        </li>
+                        <?php
+                    } else {
+                        ?>
+                        <li>
+                            <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=1&category=<?php echo $category; ?>"><?php echo "1..."; ?></a>
+                        </li>
+                        <?php
+                        for ($i = $page - 2; $i <= $page + 2; $i++) {
+                            if ($i != $page) {
+                                ?>
+                                <li>
+                                    <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=<?php echo $i; ?>&category=<?php echo $category; ?>"><?php echo $i; ?></a>
+                                </li>
+                                <?php
+                            } else {
+                                ?>
+                                <li class="active">
+                                    <a><?php echo "$i"; ?></a>
+                                </li>
+                                <?php
+                            }
+                        }
+                        ?>
+                        <li>
+                            <a href="<?php substr($_SERVER['PHP_SELF'], strrpos($_SERVER['PHP_SELF'], '/') + 1); ?>?page=<?php echo $total_page; ?>&category=<?php echo $category; ?>"><?php echo "...$total_page"; ?></a>
+                        </li>
+                        <?php
+                    }
+                }
+                ?>
             </ul>
         </div>
     </div>
