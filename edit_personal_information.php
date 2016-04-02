5dab70feaea8fd20bcb93b1b61ab69664ce48251
Ben Yan <yanwumo@outlook.com>
2016-04-02 13:02:35 +0800

    update

--- edit_personal_information.php
+++ edit_personal_information.php
@@ -6,8 +6,10 @@ $statement->bindParam(":name", $username);
 $statement->execute();
 ?>
 
+<!--
 <div class="col-md-2">
 </div>
+-->
 <div class="col-md-8">
     <form role="form" action="post_login.php" method="post">
         <div class="form-group">
@@ -28,7 +30,9 @@ $statement->execute();
         <button type="submit" class="btn btn-default">登录</button>
     </form>
 </div>
+<!--
 <div class="col-md-2">
 </div>
+-->
 
 <?php require_once("footer.php"); ?>
