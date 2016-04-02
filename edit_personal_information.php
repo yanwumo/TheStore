2850bd144a8079d6d223c2c0e5e29429cdf5141d
Ben Yan <yanwumo@outlook.com>
2016-04-02 13:00:08 +0800

    update

--- edit_personal_information.php
+++ edit_personal_information.php
@@ -1,152 +1,34 @@
-<?php require_once("pdo_init.php"); ?>
-
+<?php require_once("header.php"); ?>
 <?php
-if (!isset($_SESSION["username"])) {
-    header("Location: login.php");
-    exit();
-}
+$username = $_SESSION["username"];
+$statement = $dbh->prepare("SELECT * FROM users WHERE name = :name");
+$statement->bindParam(":name", $username);
+$statement->execute();
 ?>
-<?php require_once("pdo_init.php"); ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>The Store</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-</head>
-<body>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <nav class="navbar navbar-default" role="navigation">
-                <div class="navbar-header">
-
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-                    </button> <a class="navbar-brand" href="#">Brand</a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active">
-                            <a href="#">Link</a>
-                        </li>
-                        <li>
-                            <a href="#">Link</a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">Action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Another action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Something else here</a>
-                                </li>
-                                <li class="divider">
-                                </li>
-                                <li>
-                                    <a href="#">Separated link</a>
-                                </li>
-                                <li class="divider">
-                                </li>
-                                <li>
-                                    <a href="#">One more separated link</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="navbar-form navbar-left" role="search">
-                        <div class="form-group">
-                            <input type="text" class="form-control" />
-                        </div>
-                        <button type="submit" class="btn btn-default">
-                            Submit
-                        </button>
-                    </form>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#">Link</a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">Action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Another action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Something else here</a>
-                                </li>
-                                <li class="divider">
-                                </li>
-                                <li>
-                                    <a href="#">Separated link</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-
-            </nav>
+<div class="col-md-2">
+</div>
+<div class="col-md-8">
+    <form role="form" action="post_login.php" method="post">
+        <div class="form-group">
+            用户名: <?php echo $username; ?>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h1>
-                            The Store <small>Interface Builder for Bootstrap</small>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-10">
-                    <form role="form" action="post_login.php" method="post">
-                        <div class="form-group">
-                            用户名
-                        </div>
-                        <div class="form-group">
-                            <label for="password">密码</label>
-                            <input type="password" class="form-control" id="password">
-                        </div>
-                        <button type="submit" class="btn btn-default">登录</button>
-                    </form>
-                </div>
-                <div class="col-md-2">
-                    <form role="form" action="post_login.php" method="post">
-                        <div class="form-group">
-                            <label for="username">用户名</label>
-                            <input type="email" class="form-control" id="username" name="username">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">密码</label>
-                            <input type="password" class="form-control" id="password">
-                        </div>
-                        <button type="submit" class="btn btn-default">登录</button>
-                    </form>
-                </div>
-            </div>
+        <div class="form-group">
+            <label for="password">原密码</label>
+            <input type="password" class="form-control" id="password">
         </div>
-    </div>
+        <div class="form-group">
+            <label for="new_password">新密码</label>
+            <input type="password" class="form-control" id="new_password">
+        </div>
+        <div class="form-group">
+            <label for="re_password">验证密码</label>
+            <input type="password" class="form-control" id="re_password">
+        </div>
+        <button type="submit" class="btn btn-default">登录</button>
+    </form>
+</div>
+<div class="col-md-2">
 </div>
 
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/scripts.js"></script>
-</body>
-</html>
+<?php require_once("footer.php"); ?>
---
+++ login.php
@@ -0,0 +1,31 @@
+<?php require_once("header.php"); ?>
+<?php
+$username = $_SESSION["username"];
+$statement = $dbh->prepare("SELECT * FROM users WHERE name = :name");
+$statement->bindParam(":name", $username);
+$statement->execute();
+?>
+
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-8">
+        <form role="form" action="post_login.php" method="post">
+            <div class="form-group">
+                <label for="password">原密码</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <div class="form-group">
+                <label for="new_password">新密码</label>
+                <input type="password" class="form-control" id="new_password">
+            </div>
+            <div class="form-group">
+                <label for="re_password">验证密码</label>
+                <input type="password" class="form-control" id="re_password">
+            </div>
+            <button type="submit" class="btn btn-default">登录</button>
+        </form>
+    </div>
+    <div class="col-md-2">
+    </div>
+
+<?php require_once("footer.php"); ?>
