b4cf1fae3bdf7f0e91355969eb2b2c35f9cf046e
vicky <cuii.vv@gmail.com>
2016-04-02 16:39:10 +0800

    header jumbotron

--- view_item.php
+++ view_item.php
@@ -3,6 +3,47 @@
 <div class="col-md-2">
 </div>
 <div class="col-md-8">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" class="img-thumbnail" />
+            </div>
+            <div class="col-md-8">
+                <div class="list-group">
+                    <div class="list-group-item">
+                        物品名稱
+                    </div>
+                    <div class="list-group-item">
+                        Facebook
+                    </div>
+                    <div class="list-group-item">
+                        商品價格
+                    </div>
+                    <<div class="list-group-item">
+                        商品數量
+                    </div>
+                    <div class="list-group-item">
+                        運輸方式
+                    </div>
+                    <div class="list-group-item">
+                        運費
+                    </div>
+                    <div class="list-group-item">
+                        交易地點
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p>
+                   備註:這個蘋果是小強媽媽的嫁妝.
+                </p>
+            </div>
+        </div>
+    </div>
+
 </div>
 <div class="col-md-2">
 </div>
