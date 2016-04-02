826d1a929255566dd796eadbc446628842a5d19e
vicky <cuii.vv@gmail.com>
2016-04-02 17:42:02 +0800

    header jumbotron

---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<?php require_once("header.php"); ?>
+
+    <div class="col-md-2">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="list-group">
+                    <div class="list-group-item">
+                        首頁
+                    </div>
+                    <div class="list-group-item">
+                        商品管理
+                    </div>
+                    <div class="list-group-item">
+                        用戶管理
+                    </div>
+                    <div class="list-group-item">
+                        數據統計
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="col-md-10">
+
+    </div>
+
+
+<?php require_once("footer.php"); ?>
