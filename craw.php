b6305153ee6efbdf413a061f1414993ab1fa9bbf
Ben Yan <yanwumo@outlook.com>
2016-04-03 03:07:32 +0800

    update

---
+++ craw.php
@@ -0,0 +1,20 @@
+<?php
+require_once("pdo_init.php");
+
+$title = $_POST["title"];
+$content = $_POST["content"];
+$post_id = $_POST["post_id"];
+$facebook_name = $_POST["facebook_name"];
+$picture = $_POST["picture"];
+$price = $_POST["price"];
+$facebook_id = $_POST["facebook_id"];
+
+$statement = $dbh->prepare("INSERT INTO facebook_items (title, content, post_id, facebook_name, picture, price, facebook_id) VALUES (:title, :content, :post_id, :facebook_name, :picture, :price, :facebook_id)");
+$statement->bindValue(":title", $title);
+$statement->bindValue(":content", $content);
+$statement->bindValue("post_id", $post_id);
+$statement->bindValue("facebook_name", $facebook_name);
+$statement->bindValue("picture", $picture);
+$statement->bindValue("price", $price);
+$statement->bindValue("facebook_id", $facebook_id);
+$statement->execute();
