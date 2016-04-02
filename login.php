c3bf8cc043291878e7ba2e4c17f9e7e56faf7e64
vicky <cuii.vv@gmail.com>
2016-04-02 13:09:57 +0800

    header jumbotron

--- login.php
+++ login.php
@@ -11,18 +11,32 @@ $statement->execute();
     <div class="col-md-8">
         <form role="form" action="post_login.php" method="post">
             <div class="form-group">
-                <label for="password">原密码</label>
-                <input type="password" class="form-control" id="password">
+                <label for="name">姓名</label>
+                <input type="text" class="form-control" id="name" name="name">
             </div>
             <div class="form-group">
-                <label for="new_password">新密码</label>
-                <input type="password" class="form-control" id="new_password">
+                <label for="email">電子信箱</label>
+                <input type="text" class="form-control" id="email" name="email">
             </div>
             <div class="form-group">
-                <label for="re_password">验证密码</label>
-                <input type="password" class="form-control" id="re_password">
+                <label for="phone">電話</label>
+                <input type="text" class="form-control" id="phone" name="phone">
             </div>
-            <button type="submit" class="btn btn-default">登录</button>
+            <div class="form-group">
+                <label for="facebook">facebook</label>
+                <input type="text" class="form-control" id="facebook" name="facebook">
+            </div>
+            <div class="form-group">
+                <label for="password">密码</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div class="form-group">
+                <label for="re_password">確認密码</label>
+                <input type="password" class="form-control" id="re_password" name="re_password">
+            </div>
+
+
+            <button type="submit" class="btn btn-default">註冊</button>
         </form>
     </div>
     <div class="col-md-2">
