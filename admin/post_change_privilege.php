19f3609db5104f90ddf04c8d0e6fdffaf229d891
Ben Yan <yanwumo@outlook.com>
2016-04-03 01:13:40 +0800

    update

--- admin/post_change_privilege.php
+++ admin/post_change_privilege.php
@@ -3,6 +3,7 @@ require_once("header.php");
 
 $statement = $dbh->prepare("UPDATE users SET privilege = :privilege WHERE id = :id");
 $statement->bindValue(":privilege", $_GET['privilege']);
+$statement->bindValue(":id", $_GET['id']);
 $statement->execute();
 ?>
 <script>document.location = document.referrer;</script>
