2a6a17edb72b600743b9c0746e084052f225eee7
Ben Yan <yanwumo@outlook.com>
2016-04-03 08:32:42 +0800

    update

--- craw.php
+++ craw.php
@@ -1,13 +1,25 @@
 <?php
 require_once("pdo_init.php");
 
-$title = $_POST["title"];
 $content = $_POST["content"];
 $post_id = $_POST["post_id"];
 $facebook_name = $_POST["facebook_name"];
 $picture = $_POST["picture"];
-$price = $_POST["price"];
 $facebook_id = $_POST["facebook_id"];
+
+$temp = strstr($content, "物品標價", true);
+$temp = strstr($temp, "物品名稱");
+if (strstr($temp, "物品名稱：")) $temp = substr($temp, strlen("物品名稱："));
+else if (strstr($temp, "物品名稱: ")) $temp = substr($temp, strlen("物品名稱: "));
+else if (strstr($temp, "物品名稱:")) $temp = substr($temp, strlen("物品名稱:"));
+$title = substr($temp, 0, strrpos($temp, "\n"));
+
+$temp = strstr($content, "數量", true);
+$temp = strstr($temp, "淡江師生優惠價");
+if (strstr($temp, "淡江師生優惠價：")) $temp = substr($temp, strlen("淡江師生優惠價："));
+else if (strstr($temp, "淡江師生優惠價: ")) $temp = substr($temp, strlen("淡江師生優惠價: "));
+else if (strstr($temp, "淡江師生優惠價:")) $temp = substr($temp, strlen("淡江師生優惠價:"));
+$price = substr($temp, 0, strrpos($temp, "\n"));
 
 $statement = $dbh->prepare("INSERT INTO facebook_items (title, content, post_id, facebook_name, picture, price, facebook_id) VALUES (:title, :content, :post_id, :facebook_name, :picture, :price, :facebook_id)");
 $statement->bindValue(":title", $title);
