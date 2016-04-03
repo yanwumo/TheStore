f2f07322cfe80f21f0fa0e1dcc430c05e0949510
Ben Yan <yanwumo@outlook.com>
2016-04-03 11:22:52 +0800

    update

--- header.php
+++ header.php
@@ -12,7 +12,6 @@ require_once("pdo_init.php");
     <title>The Store</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-    <script src="search_advice.js"></script>
 
 </head>
 <body>
@@ -67,6 +66,85 @@ require_once("pdo_init.php");
                             </li>
                         </ul>
                     <?php } ?>
+                    <script type="text/javascript">
+                        function SearchSuggest(searchFuc) {
+                            var input = $('#searchtext');
+                            var suggestWrap = $('#searchsuggest');
+                            var key = "";
+                            var init = function () {
+                                input.bind('keyup', sendKeyWord);
+                                input.bind('blur', function () {
+                                    setTimeout(hideSuggest, 100);
+                                })
+                            };
+                            var hideSuggest = function () {
+                                suggestWrap.hide();
+                            };
+                            var sendKeyWord = function (event) {
+                                var valText = $.trim(input.val());
+                                if (valText == '' || valText == key) {
+                                    return;
+                                }
+                                searchFuc(valText);
+                                key = valText;
+                            };
+                            this.dataDisplay = function (data) {
+                                if (data.length <= 0) {
+                                    suggestWrap.hide();
+                                    return;
+                                }
+                                var li;
+                                var tmpFrag = document.createDocumentFragment();
+                                suggestWrap.find('ul').html('<li>您要找的是不是:</li>');
+                                for (var i = 0; i < data.length; i++) {
+                                    a=document.createElement('A');
+                                    a.href="./showItemDetail.php?id="+data[i].id;
+                                    a.innerHTML=data[i].goodsname;
+                                    li = document.createElement('LI');
+                                    li.appendChild(a);
+                                    tmpFrag.appendChild(li);
+                                }
+                                suggestWrap.find('ul').append(tmpFrag);
+                                suggestWrap.show();
+                                suggestWrap.find('li').hover(function () {
+                                    suggestWrap.find('li').removeClass('hover');
+                                    $(this).addClass('hover');
+
+                                }, function () {
+                                    $(this).removeClass('hover');
+                                }).bind('click', function () {
+                                    input.val(this.innerHTML);
+
+                                    suggestWrap.hide();
+                                });
+                            };
+                            init();
+                        }
+                        var searchSuggest = new SearchSuggest(sendKeyWordToBack);
+                        function sendKeyWordToBack(keyword) {
+                            var obj = {
+                                "keyword": keyword
+                            };
+                            $.ajax({
+                                type: "GET",
+                                url: "http://www.nthuhw.com/searchAdvice.php",
+                                async: true,
+                                data: obj,
+                                dataType: "json",
+                                success: function (data) {
+                                    var aData = [];
+                                    for (var i = 0; i < data.length; i++) {
+                                        var dataobj={};
+                                        dataobj.goodsname=data[i]["goodsname"];
+                                        dataobj.id=data[i]["id"];
+                                        aData.push(dataobj);
+                                    }
+                                    searchSuggest.dataDisplay(aData);
+                                }
+                            });
+                        }
+
+                    </script>
                 </div>
 
             </nav>
