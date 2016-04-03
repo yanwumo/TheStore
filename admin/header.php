18b9939b265372cf98be47077bdbd2b68ccc4807
Ben Yan <yanwumo@outlook.com>
2016-04-03 12:48:21 +0800

    update

--- admin/header.php
+++ admin/header.php
@@ -28,14 +28,6 @@ require_once("../pdo_init.php");
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <form class="navbar-form navbar-left" role="search">
-                        <div class="form-group">
-                            <input type="text" class="form-control" />
-                        </div>
-                        <button type="submit" class="btn btn-default">
-                            Search
-                        </button>
-                    </form>
                     <?php if (isset($_SESSION["username"])) { ?>
                         <ul class="nav navbar-nav navbar-right">
                             <li class="dropdown">
